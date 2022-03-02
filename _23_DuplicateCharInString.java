365e367ecdf10bd1371f760137595c0cd36fd459
vivekanandd1 <vivekanandd1@live.com>
2022-03-02 11:43:23 +0530

    Change in code

--- _23_DuplicateCharInString.java
+++ _23_DuplicateCharInString.java
@@ -4,20 +4,20 @@ import java.nio.charset.StandardCharsets;
 
 public class _23_DuplicateCharInString {
     public static void main(String[] args) {
-        String cc = "elephanta";
-        char[] ar = cc.toCharArray();
-        int count=0;
-        for(int i=0;i<cc.length();i++){
-            for(int j=i+1;j<cc.length();j++){
-                char ce =cc.charAt(j);
-                if(ar[i]==ar[j]){
-                    System.out.println(ar[j]+" is duplicate");
-                    count++;
-                    break;
+        String cc = "beeautifuul beach";
+        int count;
+        char sentnc[] = cc.toCharArray();
+            for(int i=0;i<sentnc.length;i++){
+                count=1;
+                for(int j=i+1;j<sentnc.length;j++){
+                    if(sentnc[j]==sentnc[i] && sentnc[j]!='0'){
+                        count++;
+                        sentnc[j]='0';
+                    }
+                }
+                if(count>1 && sentnc[i]!='0') {
+                    System.out.println(sentnc[i]);
                 }
             }
-
-
-        }
     }
 }
