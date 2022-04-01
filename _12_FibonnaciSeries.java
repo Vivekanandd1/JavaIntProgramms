206c071d51fd408b95b16ca97b1cb9c16200eb62
vivekanandd1 <vivekanandd1@live.com>
2022-04-01 08:58:39 +0530

    1_April Push

--- _12_FibonnaciSeries.java
+++ _12_FibonnaciSeries.java
@@ -3,22 +3,16 @@ package JavaIntProgramms;
 import java.util.Scanner;
 
 public class _12_FibonnaciSeries {
-    public static void main(String[] args) {//we have to print 0,1,1,2,3,5,8
-        Scanner sc = new Scanner(System.in);
-        System.out.println("enter the number");
-        int num = sc.nextInt();
-        int n1= 0,n2=1,n3;
-        int i=0;
-        while(i<num){
-//            System.out.println("_____________");
-            System.out.print(n1);
-            System.out.print(",");
-            n3 = n2 + n1;
-            n1 = n2;
-            n2 = n3;
-            i++;
 
+        public int getSum(int a, int b) {
+            int sum = a + b;
+            return sum;
         }
 
+        public static void main(String args[]) {
+
+            _12_FibonnaciSeries obj = new _12_FibonnaciSeries();
+            System.out.println(obj.getSum(10, 20));
+        }
     }
-}
+
---
+++ _28_Stringpalindrome.java
@@ -0,0 +1,30 @@
+package JavaIntProgramms;
+
+import java.util.Scanner;
+
+public class _28_Stringpalindrome {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+
+        int low = 0;
+        int high = s.length() - 1;
+        int flag = 0;
+
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high)) {
+                flag = 1;
+                break;
+            }
+
+            low++;
+            high--;
+        }
+
+        if (flag == 0)
+            System.out.println("String is a palindrome");
+        else
+            System.out.println("String is not a palindrome");
+    }
+
+}
